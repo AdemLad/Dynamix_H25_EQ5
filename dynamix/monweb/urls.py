957d58264a1afdb82f0becf50d7474f101fe11b8
weibinfan <6252200@bdeb.qc.ca>
2025-03-19 14:08:00 -0400

    url cinetique et statique

--- dynamix/monweb/urls.py
+++ dynamix/monweb/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
-]
+    path("", views.home, name="home"),
+    path("statique/", views.statique, name="statique"),
+    path("cinetique/", views.cinetique, name="cinetique")
+    ]
