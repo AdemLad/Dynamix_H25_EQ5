98a0ec7f5c498130c18596f7df4f12245eb265f2
Abdelmadjid-Ammour <6250336@bdeb.qc.ca>
2025-04-23 17:24:38 -0400

    njkfdd

--- dynamix/monweb/urls.py
+++ dynamix/monweb/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("simulations/", views.simulations, name="simulations"),
 
+    path("simulation/statique/", views.simulateurStatique, name="simulateur statique"),
+
     path("simulation/pendule/", views.pendule, name="pendule"),
     
 ]
