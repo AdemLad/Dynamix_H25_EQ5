c7829fddd9ba60ddf9516e8fe7257e429225136d
weibinfan <6252200@bdeb.qc.ca>
2025-04-23 16:59:01 -0400

    améliorer html

--- dynamix/monweb/urls.py
+++ dynamix/monweb/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("simulations/", views.simulations, name="simulations"),
 
+    path("simulation/statique/", views.simulateurStatique, name="simulateur statique"),
+
     path("simulation/pendule/", views.pendule, name="pendule"),
     
 ]
