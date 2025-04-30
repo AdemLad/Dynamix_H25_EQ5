1e99dd6659d6520fd522cf668d671c7414c8ef89
weibinfan <6252200@bdeb.qc.ca>
2025-04-30 16:15:54 -0400

    .

--- dynamix/monweb/urls.py
+++ dynamix/monweb/urls.py
@@ -7,18 +7,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("simulations/", views.simulations, name="simulations"),
 
-    path("simulation/statique/", views.simulateurStatique, name="simulateur statique"),
-
     path("simulation/pendule/", views.pendule, name="pendule"),
-    path("simulation/lancer/", views.lancer, name="lancer"),
-    path("simulation/collisions/", views.collisions, name="collisions"),
-    path("simulation/frottement/", views.frottement, name="frottement"),
-    path("simulation/poulie/", views.poulie, name="poulie"),
-    path("simulation/densite/", views.densite, name="densite"),
-    path("simulation/gavete/", views.gravite, name="gravite"),
-
     
 ]
     
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
